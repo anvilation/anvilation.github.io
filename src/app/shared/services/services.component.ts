c7a66f4025278e0ca2df1d05c71898353f3e579f
Matthew Barben <matthew.barben@driverlane.com.au>
2021-01-29 13:56:46 +1100

    build: add angular-cli-gh-pages

--- src/app/shared/services/services.component.ts
+++ src/app/shared/services/services.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faPalette, faCogs, faCode } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.scss']
 })
 export class ServicesComponent implements OnInit {
-
+  icons = {
+    design: faPalette,
+    integration: faCogs,
+    code: faCode
+  }
   constructor() { }
 
   ngOnInit(): void {
